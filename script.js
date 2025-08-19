0b7ff0c390bc5cd254f8c46450d559775d3cbe30
SplixLovesJesus <acdasdfsa@gmail.com>
2025-08-19 16:55:17 +0300

    Finished The Project

--- script.js
+++ script.js
@@ -11,5 +11,30 @@ button.addEventListener("click", function() {
   cards.appendChild(para);
   cards.className = "cards";
   para.textContent = input.value;
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  cards.appendChild(span);
+  cards.addEventListener("click", function(e) {
+}, false)
+  if(input.value === "") {
+    alert("You must write something!");
+    card.removeChild(cards);
+}
+  input.value = "";
+  saveData();
 })
 
+card.addEventListener("click", function(e) {
+if(e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+  saveData()
+})
+
+function saveData() {
+  localStorage.setItem("data", card.innerHTML);
+}
+function showData() {
+  card.innerHTML = localStorage.getItem("data");
+}
+showData();
